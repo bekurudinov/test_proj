a4f209a23d76ddc8201254c15568a5cc6926b607
Your Name <you@example.com>
2023-03-17 12:35:02 +0600

    first commit

---
+++ APP/product/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import ProductView
+
+
+router = DefaultRouter()
+router.register('', ProductView)
+
+urlpatterns = []
+urlpatterns += router.urls
+
+
